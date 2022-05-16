e959edaa86d2ea5db646120e4fe3fd84a79db8b8
Erick Giraldo <santiagogiraldo_514@hotmail.com>
2022-05-15 20:52:20 -0500

    Added requirements file

--- ask_input.py
+++ ask_input.py
@@ -1,9 +1,9 @@
 import decimal
 
-def check_first_input(number):
+def validate_binary(number):
+    pass
 
-
-def check_first_input(number):
+def check_input():
     # Check with a while loop, the input number is a float or an integer
     while True:
         frst_usr_ans = input('Please input an integer or float number: ')
@@ -17,5 +17,12 @@ def check_first_input(number):
     scnd_usr_ans = 'a'
     while scnd_usr_ans not in ['b', 'd']:
         scnd_usr_ans = input('Is it a binary (b) or decimal number (d)? (b/d): ')
+        if scnd_usr_ans not in ['b', 'd']:
+            print('Please input a valid answer')
 
-    pass
+    return frst_usr_ans, scnd_usr_ans
+
+if __name__ ==  '__main__':
+    frst_usr_ans, scnd_usr_ans = check_input()
+    print('frst_usr_ans: ', frst_usr_ans)
+    print('scnd_usr_ans: ', scnd_usr_ans)
