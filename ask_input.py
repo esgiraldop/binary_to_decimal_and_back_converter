f8d3e7d56a09baaa2ab422f6bab2b755ad60c841
Erick Giraldo <santiagogiraldo_514@hotmail.com>
2022-05-15 21:36:33 -0500

    Finished functions for asking number from user

--- ask_input.py
+++ ask_input.py
@@ -1,24 +1,69 @@
 import decimal
 
-def validate_binary(number):
-    pass
+def validate_binary(frst_usr_ans, scnd_usr_ans):
+    '''
+            Function for asking the user a number and asking if the number is decimal or binary.
+            The function also checks if correct values are entered by the user.
+            :param
+                frst_usr_ans: Float number with the number the user entered
+                scnd_usr_ans: String with two possible answers (b/d) representing the type of numnbered
+                            entered by the user. 'b' for binary, 'd' for decimal.
+            :return:
+                Boolean variable.
+                    False value allows to end the outer while loop in parent function check_input() and
+                    occurs in three scenarios:
+                        1. The user indicated the number is not binary,
+                        2. The number is binary and indeed contains 1's and 0's only
+                        3. The use indicated the number is binary, it is actually not,
+                            but the user now wants to use it as decimal
+
+                    True value represents the number is not a binary number and then it is needed to ask to the user for
+                        another number
+        '''
+    if scnd_usr_ans == 'b':
+        for i in range(2,10):
+            print(f'{i} in decimal number: {str(i) in str(frst_usr_ans)}')
+            if str(i) in str(frst_usr_ans):
+                print('This is not actually a binary number, but a decimal number.\nPlease choice an option')
+                ans = '0'
+                while ans not in ['1', '2']:
+                    ans = input('1.Enter another number\n2.Use the number as decimal:')
+                    if ans not in ['1', '2']:
+                        print('Please enter a valid answer')
+
+                if ans == '1':
+                    return True
+                else:
+                    return False
+        return False
+    else:
+        return False
 
 def check_input():
-    # Check with a while loop, the input number is a float or an integer
-    while True:
+    '''
+        Function for asking the user a number and asking if the number is decimal or binary.
+        The function also checks if correct values are entered by the user.
+        :return:
+        frst_usr_ans: Float number with the number the user entered
+        scnd_usr_ans: String with two possible answers (b/d) representing the type of numnbered
+                        entered by the user. 'b' for binary, 'd' for decimal.
+    '''
+    ans = True
+    while ans:
         frst_usr_ans = input('Please input an integer or float number: ')
         try:
             frst_usr_ans = decimal.Decimal(frst_usr_ans)
         except:
             print('Please input a valid number')
         else:
-            break
 
-    scnd_usr_ans = 'a'
-    while scnd_usr_ans not in ['b', 'd']:
-        scnd_usr_ans = input('Is it a binary (b) or decimal number (d)? (b/d): ')
-        if scnd_usr_ans not in ['b', 'd']:
-            print('Please input a valid answer')
+            scnd_usr_ans = 'a'
+            while scnd_usr_ans not in ['b', 'd']:
+                scnd_usr_ans = input('Is it a binary (b) or decimal number (d)? (b/d): ')
+                if scnd_usr_ans not in ['b', 'd']:
+                    print('Please input a valid answer')
+
+            ans = validate_binary(frst_usr_ans, scnd_usr_ans)
 
     return frst_usr_ans, scnd_usr_ans
 
